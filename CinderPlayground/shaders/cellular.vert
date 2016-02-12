006e816eebf67fa615df25333bf2ba39a9e30b87
Ilya Solovyov <astronaut.str@gmail.com>
2016-02-12 20:33:55 +0300

    sources sync

--- CinderPlayground/shaders/cellular.vert
+++ CinderPlayground/shaders/cellular.vert
@@ -6,21 +6,42 @@
 //          3 4 5
 //          6 7 8
 
-in float    inCell0_0;
-in float    inCell0_1;
-in float    inCell0_2;
-in float    inCell0_3;
-in float    inCell0_4;
-in float    inCell0_5;
-in float    inCell0_6;
-in float    inCell0_7;
-in float    inCell0_8;
+in float inCellState;
 
+in mat4 inMaskTop;
+in mat4 inMaskBot;
 
+in float inNT00; in float inNT10; in float inNT20; in float inNT30;
+in float inNT01; in float inNT11; in float inNT21; in float inNT31;
+in float inNT02; in float inNT12; in float inNT22; in float inNT32;
+in float inNT03; in float inNT13; in float inNT23; in float inNT33;
+
+in float inNB00; in float inNB10; in float inNB20; in float inNB30;
+in float inNB01; in float inNB11; in float inNB21; in float inNB31;
+in float inNB02; in float inNB12; in float inNB22; in float inNB32;
+in float inNB03; in float inNB13; in float inNB23; in float inNB33;
 
 out float   outCellState;
 
 void main()
 {
-    outCellState = inCell0 + inCell1 + inCell2 + inCell3 + inCell4 + inCell5 + inCell6 + inCell7 + inCell8;
+    mat4 neighborsTop;
+    neighborsTop[0] = vec4(inNT00, inNT10, inNT20, inNT30);
+    neighborsTop[1] = vec4(inNT01, inNT11, inNT21, inNT31);
+    neighborsTop[2] = vec4(inNT02, inNT12, inNT22, inNT32);
+    neighborsTop[3] = vec4(inNT03, inNT13, inNT23, inNT33);
+    
+    mat4 neighborsBot;
+    neighborsBot[0] = vec4(inNB00, inNB10, inNB20, inNB30);
+    neighborsBot[1] = vec4(inNB01, inNB11, inNB21, inNB31);
+    neighborsBot[2] = vec4(inNB02, inNB12, inNB22, inNB32);
+    neighborsBot[3] = vec4(inNB03, inNB13, inNB23, inNB33);
+    
+    mat4 top = transpose(inMaskTop) * inNeighborsTop;
+    mat4 bot = transpose(inMaskBot) * inNeighborsBot;
+    
+    vec4 cellsTop = vec4(top[0][0], top[1][1], top[2][2], top[3][3]);
+    vec4 cellsBot = vec4(bot[0][0], bot[1][1], bot[2][2], bot[3][3]);
+    
+    outCellState = 0.0;
 }
--- CinderPlayground/src/GPULife.cpp
+++ CinderPlayground/src/GPULife.cpp
@@ -1,4 +1,4 @@
-#include "CinderPlaygroundApp.h"
+#include "GPULife.h"
 #include "cinder/Timeline.h"
 #include "cinder/app/cocoa/PlatformCocoa.h"
 #include <fstream>
@@ -54,22 +54,54 @@ void CinderPlaygroundApp::_prepareFeedbackProgram()
     std::cerr << lastError(_feedbackShader) << std::endl;
 }
 
+const int CinderPlaygroundApp::_ruleRadius = 1;
+
+int cycledIndex(int index, int length)
+{
+    if (index < 0)
+        return cycledIndex(length + index, length);
+    else if (index >= length)
+        return cycledIndex(index - length, length);
+    else
+        return index;
+}
+
 void CinderPlaygroundApp::_prepareFeedbackBuffers()
 {
     // data format
     _gridWidth = _gridHeight = 10;
     _dataLength = _gridWidth * _gridHeight;
     _dataBytesSize = _dataLength * sizeof(GLfloat);
+    
     _dataInputBuffer = new GLfloat[_dataLength];
     _dataFeedbackBuffer = new GLfloat[_dataLength];
     for (int i = 0; i < _dataLength; ++i)
     {
         _dataFeedbackBuffer[i] = _dataInputBuffer[i] = 1;
     }
-    
-    glGenBuffers(1, &_feedbackVbo);
-    glBindBuffer(GL_ARRAY_BUFFER, _feedbackVbo);
+    glGenBuffers(1, &_vboCells);
+    glBindBuffer(GL_ARRAY_BUFFER, _vboCells);
     glBufferData(GL_ARRAY_BUFFER, _dataBytesSize, _dataInputBuffer, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    
+    CellHelper** cellsTemp = new CellHelper*[_gridWidth];
+    for (int i = 0; i < _gridWidth; ++i)
+    {
+        cellsTemp[i] = new CellHelper[_gridHeight];
+        for (int j = 0; j < _gridHeight; ++j)
+        {
+            cellsTemp[i][j] = CellHelper(i, j);
+            cellsTemp[i][j].FillNeighbors(cellsTemp, _gridWidth, _gridHeight, _ruleRadius);
+        }
+    }
+    const int sectionWidth = 8;
+    const int sectionHeigth = 4;
+    int maskLength = _gridWidth * (sectionWidth * sectionHeigth) * _gridHeight;
+    float* mask = new float[maskLength];
+
+    glGenBuffers(1, &_vboMask);
+    glBindBuffer(GL_ARRAY_BUFFER, _vboMask);
+    glBufferData(GL_ARRAY_BUFFER, maskLength, mask, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     
     glGenBuffers(1, &_feedbackTfbo);
@@ -82,18 +114,12 @@ void CinderPlaygroundApp::_prepareFeedbackVertexArray()
 {
     glGenVertexArrays(1, &_feedbackVao);
     glBindVertexArray(_feedbackVao);
-    glBindBuffer(GL_ARRAY_BUFFER, _feedbackVbo);
-    //_WTF
-    // data format
-    //GLint cellStateAttr = glGetAttribLocation(_feedbackProgram, "inCellState");
-    //glVertexAttribPointer(cellStateAttr, 1, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
-    //glEnableVertexAttribArray(cellStateAttr);
+    glBindBuffer(GL_ARRAY_BUFFER, _vboCells);
     
-    int ruleRadius = 1;
     int counter = 0;
-    for (int i = -ruleRadius; i <= ruleRadius; ++i)
+    for (int i = -_ruleRadius; i <= _ruleRadius; ++i)
     {
-        for (int j = -ruleRadius; j <= ruleRadius; j++)
+        for (int j = -_ruleRadius; j <= _ruleRadius; j++)
         {
             std::cerr << counter << endl;
             GLint cellAttrib = glGetAttribLocation(_feedbackProgram, (std::string("inCell") + toString(counter)).c_str());  //_WTF
@@ -119,7 +145,7 @@ void CinderPlaygroundApp::_prepareFeedback()
 
 void CinderPlaygroundApp::_prepareNextUpdate()
 {
-    glBindBuffer(GL_ARRAY_BUFFER, _feedbackVbo);
+    glBindBuffer(GL_ARRAY_BUFFER, _vboCells);
     glBufferSubData(GL_ARRAY_BUFFER, 0, _dataBytesSize, _dataFeedbackBuffer);                           _WTF
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
@@ -156,7 +182,7 @@ CinderPlaygroundApp::~CinderPlaygroundApp()
     glDeleteShader(_feedbackShader);
     
     glDeleteBuffers(1, &_feedbackTfbo);
-    glDeleteBuffers(1, &_feedbackVbo);
+    glDeleteBuffers(1, &_vboCells);
     
     glDeleteVertexArrays(1, &_feedbackVao);
 }
@@ -195,5 +221,72 @@ void CinderPlaygroundApp::draw()
         gl::drawString(toString(_dataFeedbackBuffer[i]), ivec2(i / _gridWidth, i - _gridWidth * (i / _gridWidth)) * ivec2(30, 30));
     }
 }
+/*
+ for (int i = 0; i < _gridWidth; ++i)
+ {
+ for (int j = 0; j < _gridHeight; ++j)
+ {
+ int indexCell = i + j * _gridWidth * sectionWidth * sectionHeigth;
+ for (int k = 0; k < sectionWidth; ++k)
+ {
+ int targetInRow = 0;
+ bool topEdge = j == 0;
+ bool botEdge = j == _gridHeight - 1;
+ bool leftEdge = i == 0;
+ bool rightEdge = i == _gridWidth - 1;
+ 
+ switch (k)
+ {
+ case 0:
+ if (leftEdge && topEdge)
+ targetInRow = 3; // AA
+ else if (leftEdge && !topEdge)
+ targetInRow = 2; // G
+ else if (!leftEdge && topEdge)
+ targetInRow = 1; // B
+ break;
+ case 1:
+ if (topEdge)
+ targetInRow = 1;
+ break;
+ case 2:
+ if (rightEdge && topEdge)
+ targetInRow = 3; // BB
+ else if (rightEdge && !topEdge)
+ targetInRow = 2; // E
+ else if (!rightEdge && topEdge)
+ targetInRow = 1; // A
+ break;
+ case 3:
+ if (leftEdge)
+ targetInRow = 1;
+ break;
+ case 4:
+ if (rightEdge)
+ targetInRow = 1;
+ break;
+ case 5:
+ break;
+ case 6:
+ if (botEdge)
+ targetInRow = 1;
+ break;
+ case 7:
+ if (rightEdge && botEdge)
+ targetInRow = 3; // CC
+ else if (rightEdge && !botEdge)
+ targetInRow = 2; // F
+ else if (!rightEdge && botEdge)
+ targetInRow = 1; // D
+ break;
+ }
+ for (int l = 0; l < sectionHeigth; ++l)
+ {
+ int sectionIndex = i + j * sectionWidth;
+ mask[indexCell + sectionIndex] = (targetInRow == l) ? 1.0 : 0.0;
+ }
+ }
+ }
+ }*/
 
 CINDER_APP( CinderPlaygroundApp, RendererGl )
